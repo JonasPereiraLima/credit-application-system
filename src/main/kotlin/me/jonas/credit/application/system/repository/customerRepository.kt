d875a6f0604ecb8755c18d354c6c8b1847caf14a
Jonas <jonas.pereira.lima1@gmail.com>
2024-02-20 14:45:34 -0300

    Classes da camada de persistência

---
+++ src/main/kotlin/me/jonas/credit/application/system/repository/customerRepository.kt
@@ -0,0 +1,8 @@
+package me.jonas.credit.application.system.repository
+
+import me.jonas.credit.application.system.entity.Customer
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface customerRepository: JpaRepository<Customer, Long>
